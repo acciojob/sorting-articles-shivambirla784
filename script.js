b698fec9b890c95e34a4c78b56e89161d78f0094
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2025-09-04 20:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const unlist = document.getElementsByTagName("ul")[0];
+const unlist = document.getElementById("bands");
 const lis = document.getElementsByTagName("li");
 const btn = document.getElementById("btn");
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
